59a4ef4b634c250222077448bf83370dcfa09083
AndreySh <shingaryovan@gmail.com>
2023-02-04 23:47:46 +1200

    найти Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов. не совсем работает, выводит целочисленный ответ, а не дробный

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+Console.Write("Введите количество строк матрицы: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m,n];
+
+void PrintMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($" {arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            arg[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void AvgMatrix(int[,] arg)
+{
+    int sum = 0;
+    double result = 0;
+    
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            sum = sum + arg[i,j];
+        }
+        Console.WriteLine("суммы столбцов: " + sum);
+        result = sum / arg.GetLength(1);
+        Console.WriteLine("average = " + result);
+    }
+    sum = 0;
+}
+
+FillMatrix(matrix);
+
+PrintMatrix(matrix);
+
+AvgMatrix(matrix);
